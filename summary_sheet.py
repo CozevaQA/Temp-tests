0e2a51de323f28e47d5e4f8e038910df2f069d1d
wdey <wdey@cozeva.com>
2022-07-11 18:45:10 +0530

    Adding test files

--- summary_sheet.py
+++
@@ -1,6 +0,0 @@
-from openpyxl import Workbook, load_workbook
-from openpyxl.utils import get_column_letter
-
-path = "C:\\Psuedo D Drive\\AnthemValidation"
-
-wb = load_workbook(path+"\\AnthemCounts.xlsx")
