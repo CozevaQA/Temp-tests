5c02f1957b02c9ab1c50175c6d14c6839c5cacb5
wdey <wdey@cozeva.com>
2022-07-18 18:31:55 +0530

    Adding testing library

--- Supplementaldatatest.py
+++ Supplementaldatatest.py
@@ -8,8 +8,8 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException
 import time
-import AutomationTestTools as testlib
+import AutomationTestTools as sf
 #test supplemental data addition and verification relating to that
 
-testlib.testlogin("https://stage.cozeva.com/patient_detail/1Y7FD8W?tab_type=CareOps&cozeva_id=1Y7FD8W&patient_id=9266593&cozeva_id=1Y7FD8W&session=YXBwX2lkPXJlZ2lzdHJpZXMmY3VzdElkPTE1MDAmZG9jdG9yc1BlcnNvbklkPTk2MjkxMTImZG9jdG9yX3VpZD05NjA5MDIzJnBheWVySWQ9MTUwMCZxdWFydGVyPTIwMjItMTItMzEmaG9tZT1ZWEJ3WDJsa1BYSmxaMmx6ZEhKcFpYTW1ZM1Z6ZEVsa1BURTFNREFtY0dGNVpYSkpaRDB4TlRBd0ptOXlaMGxrUFRFMU1EQSZmaWx0ZXJfb3JnX2lkPQ==&first_load=1")
-
+driver = sf.testlogin("https://stage.cozeva.com/patient_detail/1Y7FD8W?tab_type=CareOps&cozeva_id=1Y7FD8W&patient_id=9266593&cozeva_id=1Y7FD8W&session=YXBwX2lkPXJlZ2lzdHJpZXMmY3VzdElkPTE1MDAmZG9jdG9yc1BlcnNvbklkPTk2MjkxMTImZG9jdG9yX3VpZD05NjA5MDIzJnBheWVySWQ9MTUwMCZxdWFydGVyPTIwMjItMTItMzEmaG9tZT1ZWEJ3WDJsa1BYSmxaMmx6ZEhKcFpYTW1ZM1Z6ZEVsa1BURTFNREFtY0dGNVpYSkpaRDB4TlRBd0ptOXlaMGxrUFRFMU1EQSZmaWx0ZXJfb3JnX2lkPQ==&first_load=1")
+sf.ajax_preloader_wait(driver)
